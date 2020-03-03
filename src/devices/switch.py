a0b9e9fbddc7e908a86775d3ef70a92742de9e52
Guðmundur Björn Birkisson <gbbirkisson@gmail.com>
2020-03-03 18:37:07 +0100

    Add devices

--- src/devices/switch.py
+++ src/devices/switch.py
@@ -2,6 +2,30 @@ import logging
 
 from gpiozero import LED
 
+from hass.components import Switch
+
+
+class SSR(Switch):
+    def __init__(self, mqtt, pin=None):
+        super().__init__(mqtt, 'ssr')
+        self._on = False
+        self._led = None
+        if pin is not None:
+            self._led = LED(pin)
+
+    def state_get(self):
+        return self._on
+
+    def state_set(self, state):
+        if self._on != state:
+            logging.debug('SSR set {}'.format('ON' if on else 'OFF'))
+            self._on = state
+            if self._led is not None:
+                self._led.value = state
+
+    def __call__(self, state):
+        self.state_set_and_send(state)
+
 
 class _Switch:
     def __init__(self, pin):
--- src/devices/thermometer.py
+++ src/devices/thermometer.py
@@ -2,6 +2,63 @@ import logging
 
 from ds18b20 import DS18B20
 
+from hass.components import Sensor, Cover
+
+
+class Thermometer(Sensor):
+    def __init__(self, mqtt, id):
+        super().__init__(mqtt, 'temp_' + id.lower(), '°C')
+        self._therm = DS18B20(id)
+
+    def id_get(self):
+        return self._therm.get_id()
+
+    def state_get(self):
+        return self._therm.get_temperature()
+
+
+class ThermometerWeight(Cover):
+    def __init__(self, mqtt, id, position=100):
+        super().__init__(mqtt, id, 0, 200)
+        self._position = position
+
+    def position_set(self, value):
+        self._position = value
+
+    def position_get(self):
+        return self._position
+
+    def multiplier(self):
+        return self._position / 100
+
+
+class AverageThermometer(Sensor):
+    def __init__(self, mqtt, thermometers):
+        super().__init__(mqtt, 'temp_average', '°C')
+        self._thermometers = thermometers
+
+    def state_get(self):
+        res = 0.0
+        for t in self._thermometers:
+            res = res + t.state_get()
+        return res / len(self._thermometers)
+
+
+class WeightedAverageThermometer(Sensor):
+    def __init__(self, mqtt, thermometers_weights):
+        super().__init__(mqtt, 'temp_average_weighted', '°C')
+        self._thermometers = thermometers_weights
+
+    def state_get(self):
+        values = [therm.state_get() for therm, weight in self._thermometers]
+        weights = [weight.multiplier() for therm, weight in self._thermometers]
+
+        res = 0.0
+        for x, y in zip(values, weights):
+            res += x * y
+
+        return res / sum(self._thermometers)
+
 
 def get_thermometers():
     return _Thermometers()
