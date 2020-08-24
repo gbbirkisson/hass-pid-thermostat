63dfecd170f2ac752a9e099e16364f2dae3701e3
Guðmundur Björn Birkisson <gbbirkisson@gmail.com>
2020-08-24 17:41:43 +0200

    Fix CPU temp

--- src/devices/rpi.py
+++ src/devices/rpi.py
@@ -1,5 +1,5 @@
 def cpu_temp():
     with open('/sys/class/thermal/thermal_zone0/temp', 'r') as f:
         for line in f:
-            return float(line.strip())
+            return float(line.strip()) / 1000
     return None
